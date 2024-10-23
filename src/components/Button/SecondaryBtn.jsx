53489c5d9ab00d18cbc7464d83a65449988c414f
George Kampouris <geokamp98@gmail.com>
2024-10-23 13:33:11 +0300

    ok2

--- src/components/Button/SecondaryBtn.jsx
+++ src/components/Button/SecondaryBtn.jsx
@@ -12,7 +12,7 @@ const SecondaryBtn = ({
       <button
         type={type}
         id={id}
-        className={`text-base text-indigo-600 bg-indigo-600/5 font-medium px-3 py-1.5 rounded flex items-center border border-indigo-600 gap-x-1 hover:border-indigo-600/70 hover:bg-indigo-600/10 ease-out duration-500 ${className}`}
+        className={`text-base text-red-700 bg-red-700/5 font-medium px-3 py-1.5 rounded flex items-center border border-red-600 gap-x-1 hover:border-indigo-600/70 hover:bg-red-700/10 ease-out duration-500 ${className}`}
         onClick={onClick}
       >
         {children}
